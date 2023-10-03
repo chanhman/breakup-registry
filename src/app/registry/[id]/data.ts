461d2a657644aa3420ddf7ccbba63f1127fa0c8d
Chanh Nguyen <hello@chanh.design>
2023-10-02 23:26:34 -0600

    Move mock data

---
+++ src/app/registry/[id]/data.ts
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    id: 1,
+    name: 'White Stretchy Spiderweb Halloween Decorative Prop - Hyde & EEK! Boutique',
+    price: 4.0,
+    link: 'https://www.target.com/p/white-stretchy-spiderweb-halloween-decorative-prop-hyde-38-eek-boutique-8482/-/A-81063450',
+  },
+  {
+    id: 2,
+    name: "Halloween Stoneware Figural 14.1oz Mug 'Cat' - Hyde & EEK! Boutique",
+    price: 4.0,
+    link: 'https://www.target.com/p/halloween-stoneware-figural-14-1oz-mug-39-cat-39-hyde-38-eek-boutique-8482/-/A-87650813',
+  },
+  {
+    id: 3,
+    name: '12oz Halloween Mummy Figural Mug - Hyde & EEK! Boutique',
+    price: 4.0,
+    link: 'https://www.target.com/p/12oz-halloween-mummy-figural-mug-hyde-38-eek-boutique-8482/-/A-85298950',
+  },
+];
