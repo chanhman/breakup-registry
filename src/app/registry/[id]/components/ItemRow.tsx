ecf5c986daadbead90a5e297224fb015e12a5945
Chanh Nguyen <hello@chanh.design>
2023-10-03 11:14:15 -0600

    Display purchased checkmark for nonadmins, and purchased states

--- src/app/registry/[id]/components/ItemRow.tsx
+++ src/app/registry/[id]/components/ItemRow.tsx
@@ -4,11 +4,13 @@ import { Item } from './types';
 
 type Props = {
   data: Item;
+  isAdmin: boolean;
   setItems: Dispatch<SetStateAction<Item[]>>;
 };
 
-export default function ItemRow({ data, setItems }: Props) {
+export default function ItemRow({ data, isAdmin, setItems }: Props) {
   const [toggleEdit, setToggleEdit] = useState(false);
+  const purchased = data.purchased_status;
 
   function handleDelete(id: number) {
     setItems((prev) => prev.filter((item) => item.id !== id));
@@ -22,7 +24,7 @@ export default function ItemRow({ data, setItems }: Props) {
     <div className="py-5 border-slate-200 border-b">
       <div className="flex items-center justify-between ">
         <div className="flex gap-2 items-center text-lg">
-          {data.purchased_status && (
+          {purchased && (
             <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
               Purchased
             </span>
@@ -33,18 +35,42 @@ export default function ItemRow({ data, setItems }: Props) {
           ${data.price}
         </div>
         <div className="flex gap-4">
-          <button
-            className="pointer-events-auto ml-4 flex-none px-2 py-[0.3125rem] font-medium text-slate-700 ring-1 ring-slate-700/10 hover:bg-slate-50"
-            onClick={handleEditToggle}
-          >
-            Edit
-          </button>
-          <button
-            className="flex justify-center px-3 py-1.5 text-sm font-semibold leading-6 text-red-500 hover:text-white hover:bg-red-500 ring-1 ring-red-500"
-            onClick={() => handleDelete(data.id)}
-          >
-            Delete
-          </button>
+          {isAdmin && (
+            <>
+              <button
+                className="pointer-events-auto ml-4 flex-none px-2 py-[0.3125rem] font-medium text-slate-700 ring-1 ring-slate-700/10 hover:bg-slate-50"
+                onClick={handleEditToggle}
+              >
+                Edit
+              </button>
+              <button
+                className="flex justify-center px-3 py-1.5 text-sm font-semibold leading-6 text-red-500 hover:text-white hover:bg-red-500 ring-1 ring-red-500"
+                onClick={() => handleDelete(data.id)}
+              >
+                Delete
+              </button>
+            </>
+          )}
+          {!isAdmin && !purchased && (
+            <>
+              <div className="flex items-center">
+                <input
+                  id={data.name}
+                  name={data.name}
+                  value={data.name}
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label
+                  htmlFor={data.name}
+                  className="ml-3 min-w-0 flex-1 text-gray-500"
+                >
+                  I bought this
+                </label>
+              </div>
+            </>
+          )}
+          {!isAdmin && purchased && <div>Bought</div>}
         </div>
       </div>
       {toggleEdit && (
--- src/app/registry/[id]/page.tsx
+++ src/app/registry/[id]/page.tsx
@@ -8,31 +8,33 @@ import { data } from './data';
 
 export default function Page() {
   const searchParams = useSearchParams();
-  const isAdmin = searchParams.get('admin');
+  const isAdmin = !!searchParams.get('admin');
   const [items, setItems] = useState(data);
 
-  return isAdmin ? (
+  return (
     <div className="py-10 px-8">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl text-slate-900 sm:text-4xl">Mary's Registry</h1>
-        <div className="text-right">
-          <div>Your breakup registry link</div>
-          <div>http://localhost:3000/registry/chanh</div>
-        </div>
+        {isAdmin && (
+          <div className="text-right">
+            <div>Your breakup registry link</div>
+            <div>http://localhost:3000/registry/chanh</div>
+          </div>
+        )}
       </div>
-      <AddItemForm setItems={setItems} />
+
+      {isAdmin && <AddItemForm setItems={setItems} />}
+
       <div className="">
         {items.map((item) => (
-          <ItemRow data={item} setItems={setItems} key={item.id} />
+          <ItemRow
+            data={item}
+            isAdmin={isAdmin}
+            setItems={setItems}
+            key={item.id}
+          />
         ))}
       </div>
-    </div>
-  ) : (
-    <div className="py-10 px-8">
-      <div>
-        <h1 className="text-2xl text-slate-900 sm:text-4xl">Mary's Registry</h1>
-      </div>
-      <div className="my-8">Non-admin</div>
     </div>
   );
 }
