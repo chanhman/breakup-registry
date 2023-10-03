f16a9fefd70d4bad80c8388c7f7a751602fd8797
Chanh Nguyen <hello@chanh.design>
2023-10-03 11:45:32 -0600

    Add static filters

--- src/app/registry/[id]/page.tsx
+++ src/app/registry/[id]/page.tsx
@@ -25,6 +25,20 @@ export default function Page() {
 
       {isAdmin && <AddItemForm setItems={setItems} />}
 
+      <div>
+        <select name="items">
+          <option value="">All items</option>
+          <option value="purchased">Purchased items</option>
+          <option value="unpurchased">Unpurchased items</option>
+        </select>
+        <select name="items">
+          <option value="">Price</option>
+          <option value="under-50">Under $50</option>
+          <option value="under-100">Under $100</option>
+          <option value="above-100">Above $100</option>
+        </select>
+      </div>
+
       <div className="">
         {items.map((item) => (
           <ItemRow
