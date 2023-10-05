89b47312056389b19818956a29bef9b4d096908b
Chanh Nguyen <hello@chanh.design>
2023-10-05 17:53:56 -0600

    Basic login styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
 import { Database } from '@/lib/types/supabase';
+import Label from '../components/Label';
+import Input from '../components/Input';
 
 export default function Login() {
   const [email, setEmail] = useState('');
@@ -37,21 +39,56 @@ export default function Login() {
   };
 
   return (
-    <>
-      <input
-        name="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <input
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <button onClick={handleSignIn}>Sign in</button>
-      <button onClick={handleSignUp}>Sign up</button>
-      <button onClick={handleSignOut}>Sign out</button>
-    </>
+    <div className="flex min-h-[840px] flex-col bg-white">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            <h2>Sign in</h2>
+          </div>
+          <div className="grid gap-6 mt-12">
+            <div>
+              <Label text="Email" htmlFor="email" />
+              <Input
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </div>
+            <div>
+              <Label text="Password" htmlFor="password" />
+              <Input
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </div>
+            <div>
+              <button
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleSignIn}
+              >
+                Sign in
+              </button>
+            </div>
+            <div>
+              <button
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleSignUp}
+              >
+                Sign up
+              </button>
+            </div>
+            <div>
+              <button
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={handleSignOut}
+              >
+                Sign out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
