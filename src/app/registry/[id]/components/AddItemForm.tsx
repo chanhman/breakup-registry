721832cabcd04b020fa092282a07508c545bccd2
Chanh Nguyen <hello@chanh.design>
2023-10-04 18:58:48 -0600

    Add category to add form

--- src/app/registry/[id]/components/AddItemForm.tsx
+++ src/app/registry/[id]/components/AddItemForm.tsx
@@ -8,7 +8,14 @@ type Props = {
 };
 
 export default function AddItemForm({ userId }: Props) {
-  const [formData, setFormData] = useState({ name: '', price: 0, link: '' });
+  const initialFormData = {
+    name: '',
+    price: 0,
+    link: '',
+    category_id: 'other',
+  };
+  const [formData, setFormData] = useState(initialFormData);
+
   const supabase = createClientComponentClient();
 
   async function handleOnSubmit(e: FormEvent<HTMLFormElement>) {
@@ -22,7 +29,7 @@ export default function AddItemForm({ userId }: Props) {
           name: formData.name,
           link: formData.link,
           price: formData.price,
-          category_id: 'other',
+          category_id: formData.category_id,
         },
       ])
       .select();
@@ -31,9 +38,12 @@ export default function AddItemForm({ userId }: Props) {
       console.log('There was an error');
     }
     console.log('Submitted');
+    setFormData(initialFormData);
   }
 
-  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+  function handleInputChange(
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) {
     setFormData((prev) => ({
       ...prev,
       [e.target.name]:
@@ -73,6 +83,20 @@ export default function AddItemForm({ userId }: Props) {
           value={formData.link}
           required
         />
+      </div>
+      <div className="flex-1 grid gap-2">
+        <Label text="Category" htmlFor="category" />
+        <select
+          className="ring-1 ring-inset ring-gray-300"
+          onChange={(e) => handleInputChange(e)}
+          value={formData.category_id}
+          name="category_id"
+          id="category"
+        >
+          <option value="other">Other</option>
+          <option value="bedroom">Bedroom</option>
+          <option value="kitchen">Kitchen</option>
+        </select>
       </div>
       <div>
         <button
